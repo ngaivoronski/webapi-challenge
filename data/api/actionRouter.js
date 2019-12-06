95738948ad5a93a75378197bc1b545fa37ad9329
ngaivoronski <ngaivoronski@gmail.com>
2019-12-06 12:20:23 -0500

    action router complete

--- data/api/actionRouter.js
+++ data/api/actionRouter.js
@@ -6,8 +6,23 @@ const actionDb = require('../helpers/actionModel');
 
 // custom middleware
 
+function validateActionID(req, res, next) {
+    actionDb.get(req.params.id)
+        .then(act => {
+            if(act && act.id) {
+                next();
+            } else {
+                res.status(400).json({  message: "Action ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Database error." });
+        });
+}
 
 // CRUD
+
+// Get a list of actions from the action database
 
 router.get('/', (req, res) => {
     actionDb.get()
@@ -17,6 +32,18 @@ router.get('/', (req, res) => {
         .catch(err => {
         res.status(500).json({ error: "Actions could not be retrieved." });
         })
+});
+
+// Get a specific action by ID (disregarding project id)
+
+router.get('/:id', validateActionID, (req, res) => {
+    actionDb.get(req.params.id)
+    .then(action => {
+    res.status(200).json(action);
+    })
+    .catch(err => {
+    res.status(500).json({ error: "Action could not be retrieved." });
+    })
 });
 
 module.exports = router;
--- data/api/projectRouter.js
+++ data/api/projectRouter.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const projectDb = require('../helpers/projectModel');
+const actionDb = require('../helpers/actionModel');
 
-// custom middleware
+// Custom middleware
 
 function validateProjectID(req, res, next) {
     projectDb.get(req.params.id)
@@ -34,6 +35,34 @@ function validateProject(req, res, next) {
     }
 }
 
+function validateAction(req, res, next) {
+    if(req.body && req.body.description && req.body.notes) {
+        next();
+    } else if (req.body && req.body.description) {
+        res.status(400).json({ message: "Missing required notes field." });
+    } else if (req.body && req.body.notes) {
+        res.status(400).json({ message: "Missing required description field." });
+    } else if (req.body) {
+        res.status(400).json({ message: "Missing required description and notes fields." });
+    } else {
+        res.status(400).json({ message: "Missing action data." })
+    }
+}
+
+function validateProjActionID(req, res, next) {
+    actionDb.get(req.params.actionID)
+        .then(act => {
+            if(act && act.id) {
+                next();
+            } else {
+                res.status(400).json({  message: "Action ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Database error." });
+        });
+}
+
 // CRUD
 
 // Get a list of projects
@@ -56,7 +85,7 @@ router.get('/:id', validateProjectID, (req, res) => {
     res.status(200).json(project);
     })
     .catch(err => {
-    res.status(500).json({ error: "Projects could not be retrieved." });
+    res.status(500).json({ error: "Project could not be retrieved." });
     })
 });
 
@@ -80,7 +109,7 @@ router.put('/:id', validateProjectID, validateProject, (req, res, next) => {
         res.status(201).json(project);
     })
     .catch(err => {
-        res.status(500).json({ error: "There was an error adding the project." });
+        res.status(500).json({ error: "There was an error editing the project." });
     });
 });
 
@@ -96,7 +125,63 @@ router.delete('/:id', validateProjectID, (req, res, next) => {
         }
     })
     .catch(err => {
+        res.status(500).json({ error: "There was an error deleting the project." });
+    });
+});
+
+// Add an action
+
+router.post('/:id/actions', validateProjectID, validateAction, (req, res, next) => {
+    const newAction = {...req.body, project_id: req.params.id}
+
+    actionDb.insert(newAction)
+    .then(action => {
+        res.status(201).json(action);
+    })
+    .catch(err => {
         res.status(500).json({ error: "There was an error adding the project." });
+    });
+});
+
+// Get a specific action by project ID and action ID
+
+router.get('/:id/actions/:actionID', validateProjActionID, (req, res) => {
+    actionDb.get(req.params.actionID)
+    .then(action => {
+    res.status(200).json(action);
+    })
+    .catch(err => {
+    res.status(500).json({ error: "Action could not be retrieved." });
+    })
+});
+
+// Edit a specific action in a specific project
+
+router.put('/:id/actions/:actionID', validateProjectID, validateProjActionID, validateAction, (req, res, next) => {
+    const editAction = {...req.body, project_id: req.params.id}
+
+    actionDb.update(req.params.actionID, editAction)
+    .then(action => {
+        res.status(201).json(action);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "There was an error editing the action." });
+    });
+});
+
+// Delete a specific action in a specific project
+
+router.delete('/:id/actions/:actionID', validateProjectID, validateProjActionID, (req, res, next) => {
+    actionDb.remove(req.params.actionID)
+    .then(confirmation => {
+        if (confirmation > 0) {
+            res.status(201).json({ message: "Action was successfully deleted." });
+        } else {
+            res.status(500).json({ error: "There was an error deleting the action." });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "There was an error deleting the action." });
     });
 });
 
